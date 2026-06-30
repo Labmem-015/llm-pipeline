f9d389576450c6014bb79575e6d275b51e9319d3
Vladlen Naumov <naumov.vlad04@gmail.com>
2026-06-30 20:26:27 +0300

    Request data model and process request func

--- src/server.py
+++ src/server.py
@@ -3,12 +3,26 @@ Here's implemented a server for interacting with user.
 '''
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Request(BaseModel):
+    username: str | None = None
+    prompt: str
+    think: bool = False
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
+    """Simple example of fastapi work."""
     return {"message": "Hello, World!"}
+
+@app.post("/chat/{session_id}")
+async def process_request(session_id: int, req: Request):
+    """Handling user request and passing it to llm-server."""
+    print("User's request is: ", req.prompt)
+    print("Session id is: ", session_id)
+    return {"status": "Processed."}
 
 # if __name__ == "__main__":
 #     import uvicorn
